7528454513ff72ec8373efff96caea9acc3cefb7
SaintNail <34795402+SaintNail@users.noreply.github.com>
2018-03-10 19:45:45 +0200

    inwork

---
+++ Lesson1/src/com/gmail/saintnail/Main.java
@@ -0,0 +1,11 @@
+package com.gmail.saintnail;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println(7 + 3);
+	}
+
+}
